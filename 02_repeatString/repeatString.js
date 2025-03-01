e239ce8c4fae5c2a3af486c74be9a08815a8796e
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-02 03:55:58 +0530

    Solve exercises 1 and 2 and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringValue, frequency) {
+    if(frequency < 0) return "ERROR";
+    let result = "";
+    for(let i = 1; i <= frequency; i++) {
+        result += stringValue;
+    }
+    return result;
 };
 
 // Do not edit below this line
