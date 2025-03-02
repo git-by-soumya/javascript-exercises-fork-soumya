6a32dca3be50687581d6bc17eeec5be308847876
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-03 01:26:41 +0530

    Solve exercise 7 tempConversion and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let result = (temperatureInFahrenheit - 32) * (5/9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let result = temperatureInCelsius * (9/5) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
