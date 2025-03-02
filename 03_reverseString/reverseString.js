333ec15a6822d9fc198f9db63331dab164203368
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-02 20:17:05 +0530

    Solve exercise 3 reverseString and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = (stringValue) => stringValue.split("").reverse().join("");
+    /*let result = "";
+    let negativeFinalIndex = stringValue.length * (-1);
+    for(let i = -1; i >= negativeFinalIndex; i--) {
+        result += stringValue.at(i);
+    }*/
 
-};
+    /*result = stringValue.split("").reverse().join("");
+    return result;
+    */
+
 
 // Do not edit below this line
 module.exports = reverseString;
