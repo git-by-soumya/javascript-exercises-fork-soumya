339a7a63cb665df7ea2b292f3c664aef042df035
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-02 23:20:13 +0530

    Solve exercise 5 sumAll and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    if( !Number.isInteger(start) || 
+        !Number.isInteger(end) ) {
+        return "ERROR";
+    }
+
+    if(start > end) {
+        /*start = start + end;
+        end = start - end;
+        start = start - end;*/
+        [start, end] = [end, start]; // array destructuring
+    }
+
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
