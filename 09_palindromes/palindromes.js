e959fc0efb5228b0695b5ddda3cc7629fa28a0a8
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-12 21:24:35 +0530

    Solve exercise 9 palindromes and pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringOnlyLowerLetters =
+     string
+     .toLowerCase()
+     .split("")
+     .filter(char => 
+        char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122 ||
+     char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57)
+     .join("");
+    
+    let stringReverse = stringOnlyLowerLetters.split("").reverse().join("");
+    
+    return stringOnlyLowerLetters === stringReverse;
 };
 
 // Do not edit below this line
