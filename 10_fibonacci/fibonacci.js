c6ef199a1c0b90e86dfad786bb976a845a6ca7c1
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-12 21:37:52 +0530

    Solve exercise 10 fibonacci and pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if(n === 1 || n === 2) {
+        return 1;
+    }
+    else if(n < 0) {
+        return "OOPS";
+    }
+    let a = b = 1;
+    let result = 0;
+    for(let i = 3; i <= n; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
