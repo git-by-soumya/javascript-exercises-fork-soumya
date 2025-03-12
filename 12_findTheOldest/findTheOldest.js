cadea03954cb7870d3b2a72f2d3fc5d0ca26fd9e
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-12 22:28:43 +0530

    Solve exercise 12 findTheOldest and pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        let oldestAge, currentAge;
+        if("yearOfDeath" in oldest) {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+        else {
+            oldestAge = new Date().getFullYear() - oldest.yearOfBirth;
+        }
 
-};
+        if("yearOfDeath" in current) {
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        }
+        else {
+            currentAge = new Date().getFullYear() - current.yearOfBirth;
+        }
+        
+        let older = currentAge > oldestAge ? current : oldest;
+        return older;
+    }
+);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
